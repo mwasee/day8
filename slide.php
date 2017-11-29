dd4bbe67861c5a0f4bd9b81f100d8c06eca57d70
mwasee68 <mwasee68@gmail.com>
2017-11-29 19:58:00 +0530

    first commit

---
+++ slide.php
@@ -0,0 +1,16 @@
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="image/63001782736p.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="..//day8/image/cap_3.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="image/chandra.jpg" alt="Third slide">
+    </div>
+  </div>
+</div>
+        
+
